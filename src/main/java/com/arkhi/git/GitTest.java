a4a78ba6c18289ec239f437709b968f365d2e153
Arkhi <hhzwnhal@outlook.com>
2024-01-13 21:55:28 +0800

    first commit

---
+++ src/main/java/com/arkhi/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.arkhi.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * ClassName:GitTest
+ * Description:
+ * Author:JiaKang Han
+ * Date:2024/1/13-21:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
